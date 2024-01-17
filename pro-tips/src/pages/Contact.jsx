36390f68b49d4e79b784140079b2559b1fa03541
HarshwardhanKalvium <145761267+HarshwardhanKalvium@users.noreply.github.com>
2024-01-17 15:13:08 +0530

    Add files via upload

---
+++ pro-tips/src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Contact = () => {
+    return(
+        <h1 style={{textAlign:"center",color:"black", position:"absolute", top:"100px",left:"43%"}}>Contact</h1>
+    )
+}
+
+export default Contact
---
+++ pro-tips/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <h1 style={{textAlign:"center",color:"black", position:"absolute", top:"100px",left:"43%"}}>Home</h1>
+    )
+}
+
+export default Home
